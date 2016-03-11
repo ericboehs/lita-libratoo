24f2314d103f53908dae074aa345773d59f66e6a
Eric Boehs <ericboehs@gmail.com>
2016-03-11 13:42:59 -0600

    Add get/search to route; Fix start_time breaking everything

--- lib/lita/handlers/libratoo.rb
+++ lib/lita/handlers/libratoo.rb
@@ -5,11 +5,12 @@ module Lita
       config :email, required: true
       config :api_key, required: true
 
-      route(/^librato\s(.+)/, :librato, command: true, help: {
-        "librato [metric] [options]" => "example: 'lita librato AWS.RDS.ReplicaLag source: * count: 5'"
+      route(/^librato get\s(.+)/, :librato_get, command: true, help: {
+        "librato get [metric] [options]" =>
+          "example: 'lita librato get AWS.RDS.ReplicaLag source: * count: 5 start_time: 2016-03-11-02:00'"
       })
 
-      def librato(response)
+      def librato_get(response)
         Librato::Metrics.authenticate config.email, config.api_key
 
         arguments = response.matches[0][0].delete(':').split
@@ -29,7 +30,9 @@ module Lita
         metric, options = arguments[0].to_sym, arguments[1..-1]
         options = symbolize_keys Hash[*options]
         options = { count: 1 }.merge options
-        options[:start_time] = DateTime.parse(options[:start_time]).to_time unless options[:start_time].is_a? Numeric
+        if options.key?(:start_time) && !options[:start_time].is_a?(Numeric)
+          options[:start_time] = DateTime.parse(options[:start_time]).to_time
+        end
 
         results = Librato::Metrics.get_measurements metric, options
         results = Hash[results.map {|source, r| [source, r.map{|v| v["value"] }]}]
@@ -42,6 +45,26 @@ module Lita
           response_text = results.map{|k,v| "#{k}: #{v.join ', '}" }.join "\n"
         end
 
+        if response_text.length > 400 && !response.message.source.private_message
+          return response.reply "Large result set. Sending you a direct message."
+          return response.reply_privately response_text
+        else
+          return response.reply response_text
+        end
+      end
+
+      route(/^librato search\s(.+)/, :librato_search, command: true, help: {
+        "librato search [metric]" => "example: 'lita librato search replica'"
+      })
+
+      def librato_search(response)
+        Librato::Metrics.authenticate config.email, config.api_key
+
+        arguments = response.matches[0][0].delete(':').split
+
+        metrics = Librato::Metrics.metrics.map{|v| v["name"] }
+        response_text = metrics.grep(/#{arguments[1]}/i).join(', ')
+        response_text = "No results" if response_text.empty?
         if response_text.length > 400 && !response.message.source.private_message
           return response.reply "Large result set. Sending you a direct message."
           return response.reply_privately response_text
